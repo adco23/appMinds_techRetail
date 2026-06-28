f6069397068e502ebd07d39a7a1e6fdf49bbab64
LucasMonteras <lucasmonteras@gmail.com>
2026-06-28 09:44:11 -0300

    se agregan ajustes en rutas de product y vistas

--- routes/index.js
+++ routes/index.js
@@ -15,7 +15,6 @@ const router = Router();
 // Vistas
 router.use('/', viewRoutes);
 router.use('/stores', storeRoutes);
-router.use('/products', productRoutes);
 
 // API
 router.use('/api/commerces', commerceRoutes);
--- routes/product.routes.js
+++ routes/product.routes.js
@@ -3,32 +3,16 @@ const router = express.Router();
 
 import {
   getProducts,
-  getProductsView,
-  getProductDetailView,
-  getProductEditView,
-  getProductNewView,
   getProductById,
   createProduct,
-  createProductFromView,
   updateProduct,
-  updateProductFromView,
   deleteProduct,
-  deleteProductFromView,
 } from '../controllers/product.controller.js';
 
 router.get('/', getProducts);
-router.get('/view', getProductsView);
-router.get('/view/new', getProductNewView);
-router.get('/view/new/:storeId', getProductNewView);
-router.get('/view/:id', getProductDetailView);
-router.get('/edit/:id', getProductEditView);
-
 router.get('/:id', getProductById);
 
 router.post('/', createProduct);
-router.post('/view', createProductFromView);
-router.post('/edit/:id', updateProductFromView);
-router.post('/delete/:id', deleteProductFromView);
 
 router.put('/:id', updateProduct);
 router.delete('/:id', deleteProduct);
